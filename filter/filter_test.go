6acf9ea6b7250e5e6422e27d183e9839944c11bb
Marcelo Glezer <marcelo.glezer@gmail.com>
2015-04-12 13:30:08 -0300

    added empty filter test

--- filter/filter_test.go
+++ filter/filter_test.go
@@ -66,3 +66,20 @@ func TestCommandMatch(t *testing.T) {
 	}
 
 }
+
+func TestNoFilterMatch(t *testing.T) {
+	var op aof.Operation
+	var ftr Filter
+	op.Command = "SELECT"
+
+	// empty filter match
+	if !Match(op, ftr, false) {
+		t.Errorf("Op.Command '%s' should match '%s'", op.Command, ftr.Command)
+		return
+	}
+	// inverse match => false
+	if Match(op, ftr, true) {
+		t.Errorf("inverse of a matching filter should return false")
+		return
+	}
+}
